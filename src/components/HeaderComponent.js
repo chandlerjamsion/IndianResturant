5671dbec1f129c588bd2f99eb2001a00e43165c8
chandlerjamsion <53658249+chandlerjamsion@users.noreply.github.com>
2019-10-03 10:15:39 -0700

    React-Redux forms and validation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -62,7 +62,7 @@ class Header extends Component {
                                 <NavItem>
                                     <Button outline onClick={this.toggleModal}><span className="fa fa-sign-in fa-lg"></span> Login</Button>
                                 </NavItem>
-                            </Nav>k
+                            </Nav>
                         </Collapse>
                     </div>
                 </Navbar>
